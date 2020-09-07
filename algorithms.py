fb390996100aa822a80b8e9715a7c45c17c9fad3
wilantury <ventas@antury.net>
2020-09-07 14:27:09 -0500

    [ADD] base interface

---
+++ algorithms.py
@@ -0,0 +1,28 @@
+
+
+
+
+if __name__ == "__main__":
+    while True:
+        print("[1] Average 2 numbers")
+        print("[2] Get month")
+        print("[3] Math Operations")
+        print("[4] Greater number")
+        print("[5] Avergage 3 numbers")
+        print("[6] Greater age")
+        option = int(input("Choice an option"))
+        if option == 1:
+            pass
+        elif option == 2:
+            pass
+        elif option == 3:
+            pass
+        elif option == 4:
+            pass
+        elif option == 5:
+            pass
+        elif option == 6:
+            pass
+        else:
+            print("Wrong option selected, please try again")
+
